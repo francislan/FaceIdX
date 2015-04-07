6d9d586560d6c7f135ae28bfcfa02eaf407bee4d
Francis Lan <francis.lan16@gmail.com>
2015-04-07 12:43:45 -0400

    Working - add TODO

--- code/include/database.h
+++ code/include/database.h
@@ -4,4 +4,7 @@
 unsigned char * loadImage(char *filename, int *w, int *h, int *comp, int req_comp);
 void freeImage(unsigned char *data);
 
+//TODO: structure for image (with w, h, n, etc)
+
+
 #endif
