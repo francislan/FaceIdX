650338ca5214a6ad3340063fb156280085618904
Francis Lan <francis.lan16@gmail.com>
2015-04-07 15:56:51 -0400

    data structures

--- code/include/database.h
+++ code/include/database.h
@@ -1,10 +1,40 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
-unsigned char * loadImage(char *filename, int *w, int *h, int *comp, int req_comp);
+struct Image loadImage(char *filename, int *w, int *h, int *comp, int req_comp);
 void freeImage(unsigned char *data);
+int get_pixel(struct Image image, int x, int y, int comp);
+
+int create_dataset(char *directory, char *dataset_path, char *name);
+int load_dataset(char *dataset_path);
+int save_average_to_dataset(struct Dataset dataset, struct Image average);
+int save_dataset_to_disk(struct Dataset dataset, char *path);
+
 
 //TODO: structure for image (with w, h, n, etc)
+struct Image {
+	unsigned char *data;
+	int w;
+	int h;
+	int comp;
+	int req_comp;
+	char *filename;
+}
 
+struct Dataset {
+	char *name;
+	int num_eigenfaces;
+	int w;
+	int h;
+	unsigned char *average;
+	unsigned char *eigenfaces; //malloc
+	struct FaceCoordinates *faces; //malloc
+}
+
+struct FaceCoordinates {
+	char *name;
+	int num_eigenfaces;
+	float *coordinates; //malloc
+}
 
 #endif
--- code/include/eigen.h
+++ code/include/eigen.h
@@ -1,6 +1,24 @@
 #ifndef EIGEN_H
 #define EIGEN_H
 
-int count_prime(int argc, char **argv);
+struct Image compute_average_gpu(struct Image *images, int num_images);
+struct Image compute_average_cpu(struct Image *images, int num_images);
 
 #endif
+
+
+// pixel_grey = get_pixel(images[0], 0, 2, 0);
+
+/*
+struct Image *images;
+// load images
+int num_images = 100;
+
+
+struct Image average;
+
+average = compute_average_gpu(images, num_images);
+
+save_average_to_dataset()
+
+*/
