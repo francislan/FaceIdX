5adc8f1dbaa504f30333d19de71258e2f47441db
Francis Lan <francis.lan16@gmail.com>
2015-04-09 13:45:45 -0400

    fix

--- code/include/misc.h
+++ code/include/misc.h
@@ -1,6 +1,13 @@
 #ifndef MISC_H
 #define MISC_H
 
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KWHT  "\x1B[37m"
+
 #define PRINT(level, fmt, ...) \
 do { \
 	if (!strcmp(level, "BUG")) { \
@@ -19,13 +26,6 @@ do { \
 		printf(KNRM fmt, ##__VA_ARGS__); \
 	} \
 } while(0)
-
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KWHT  "\x1B[37m"
 
 #define TEST_MALLOC(p) \
 do { \
