095251ce3c2eddf8915ba95da218c8d8039ea521
Francis Lan <francis.lan16@gmail.com>
2015-04-09 13:44:08 -0400

    fix

--- code/include/misc.h
+++ code/include/misc.h
@@ -1,21 +1,6 @@
 #ifndef MISC_H
 #define MISC_H
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KWHT  "\x1B[37m"
-
-#define TEST_MALLOC(p) \
-do { \
-	if ((p) == NULL) { \
-		printf("ERROR: malloc failed\n"); \
-		exit(1); \
-	} \
-} while(0)
-
 #define PRINT(level, fmt, ...) \
 do { \
 	if (!strcmp(level, "BUG")) { \
@@ -34,5 +19,21 @@ do { \
 		printf(KNRM, fmt, ##__VA_ARGS__); \
 	} \
 } while(0)
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KWHT  "\x1B[37m"
+
+#define TEST_MALLOC(p) \
+do { \
+	if ((p) == NULL) { \
+		printf("ERROR: malloc failed\n"); \
+		exit(1); \
+	} \
+} while(0)
+
 
 #endif
